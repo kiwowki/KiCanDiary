88c059b99abb9f4260a4863eca3785289c8f4402
imjonghan <danziro57@gmail.com>
2024-01-04 15:57:21 +0900

    new

--- client/src/components/diary/Write.jsx
+++ client/src/components/diary/Write.jsx
@@ -6,10 +6,8 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
 const Write = () => {
-    const [value, setValue] = useState('')
     const quillRef = useRef(null)
-    const [title, setTitle] = useState();
-
+   
     const modules = {
         toolbar: [
             [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -20,6 +18,113 @@ const Write = () => {
         ],
     }
 
+
+    // 구글 api부분
+    const [word, setWord] = useState('')
+    const [translation, setTranslation] = useState('')
+    
+    useEffect(() => {
+        console.log(translation)
+    }, [translation])
+    // 검색창 글씨가 바뀔때마다 word값이 바뀜
+
+    const handleChange = (event) => {
+        setWord(event.target.value)
+        console.log(event)
+    }
+    // 엔터누르면 구글 api를 통해 결과값 제공
+    const handleKeyPress = async (event) => {
+        // word 상태 변수가 변경되었는지 확인합니다.
+        if (event.key === 'Enter' && word !== '') {
+            console.log(word)
+            try {
+                const response = await axios.post('/api/translate', {
+                    search: word,
+                })
+                if (response.data.success) {
+                    if (response.data.data.trans) {
+                        const translatedText = response.data.data.trans
+                        setTranslation((prevTranslation) => [
+                            translatedText,
+                            ...prevTranslation,
+                        ])
+                    }
+                    if (
+                        response.data.data.dict &&
+                        response.data.data.dict[0].entry
+                    ) {
+                        const translatedTextArray =
+                            response.data.data.dict[0].entry.map(
+                                (item) => item.word
+                            )
+                        setTranslation((prevTranslation) => [
+                            ...prevTranslation,
+                            ...translatedTextArray,
+                        ])
+                    }
+                    console.log(setTranslation)
+                } else {
+                    alert('번역 실패')
+                }
+            } catch (error) {
+                console.error('번역 요청 에러:', error)
+            }
+        }
+    }
+
+    // Ginger api 부분
+    const [value, setValue] = useState('')
+
+    const [correctionsData, setCorrectionsData] = useState([])
+
+    const mainhandleKeyPress = async (event) => {
+        try {
+            if (event.key === 'Enter' && !event.shiftKey) {
+                event.preventDefault()
+                const textValue = quillRef.current?.getEditor().getText()
+                console.log(textValue)
+                setValue(textValue)
+
+                const response = await axios.post('/api/ginger', {
+                    search: value,
+                })
+                if (response.data.success) {
+                    // console.log(response.data.data.GingerTheDocumentResult.Corrections);
+
+                    const newCorrectionsData =
+                        response.data.data.GingerTheDocumentResult.Corrections.map(
+                            (correction) => ({
+                                mistakeText: correction.MistakeText,
+                                suggestions: correction.Suggestions.map(
+                                    (suggestion) => ({
+                                        text: suggestion.Text,
+                                        confidence: suggestion.Confidence,
+                                        category:
+                                            suggestion.CategoryDescription,
+                                    })
+                                ),
+                            })
+                        )
+
+                    console.log(newCorrectionsData)
+                    setCorrectionsData(newCorrectionsData)
+                } else {
+                    alert('문법 요청 실패')
+                }
+            }
+        } catch (err) {
+            console.log('문법 요청 에러:', err)
+        }
+    }
+
+    const [title, setTitle] = useState();
+    const {date} = useParams()
+    const [dateObj, setDate] = useState({
+        year: '',
+        month: '',
+        day: ''
+    })
+    
     const handleSave = async (e) => {
         e.preventDefault();
         if(title && value) {
@@ -31,7 +136,7 @@ const Write = () => {
                 content: content.ops[0]
             }
 
-            axios.post("/api/post", body).then((res)=> {
+            axios.post("/api/post/write", body).then((res)=> {
                 if(res.status.success) {
                     alert("작성 성공")
                 } 
@@ -44,13 +149,7 @@ const Write = () => {
             alert("실패")
         }
     }
-
-    const {date} = useParams()
-    const [dateObj, setDate] = useState({
-        year: '',
-        month: '',
-        day: ''
-    })
+    
     
 
     useEffect(() => {
@@ -79,12 +178,6 @@ const Write = () => {
                             </article>
                             <article className="content">
                                 <h2>Story</h2>
-                                {/* <textarea
-                                    name="content"
-                                    id="content"
-                                    cols="100"
-                                    rows="25"
-                                ></textarea> */}
                                 <ReactQuill
                                     style={{ height: '530px', marginRight: '4px', marginLeft: '4px' }}
                                     ref={quillRef}
@@ -98,19 +191,85 @@ const Write = () => {
                         </section>
                         <section className="right ml_30">
                             <h2 className="blind">서치, 오류 결과 섹션</h2>
-                            <div className='search__wrap'><span></span><input type="text" className="word__search" /></div>
+                            <div className="search__wrap">
+                                <span></span>
+                                <input
+                                    type="text"
+                                    className="word__search"
+                                    onKeyPress={handleKeyPress}
+                                    onChange={handleChange}
+                                />
+                            </div>
                             <div className="result">
                                 <h2>Result</h2>
                                 <div>
-                                    <span>splendid</span>
-                                    <span>madgnificant</span>
-                                    <span>elegant</span>
-                                    <span>grand</span>
+                                    <div className="result_wrap">
+                                        {Array.isArray(translation) &&
+                                            translation.map((result, index) => (
+                                                <div key={index}>
+                                                    {result}
+                                                    <div>
+                                                        {/* 추가로 표시할 내용이 있다면 여기에 작성 */}
+                                                    </div>
+                                                </div>
+                                            ))}
+                                    </div>
                                 </div>
                             </div>
                             <div className="correction">
                                 <h2>Correction</h2>
-                                <div></div>
+                                <div>
+                                    <ul className="correction_wrap">
+                                        {correctionsData.map(
+                                            (correction, index) => (
+                                                <li key={index}>
+                                                    <div className="wrong">
+                                                        <p className="wrong__word">
+                                                            <Link to="/write">
+                                                                {
+                                                                    correction.mistakeText
+                                                                }
+                                                            </Link>
+                                                        </p>
+                                                    </div>
+                                                    <span className="arrow"></span>
+                                                    <div className="correct">
+                                                        <p className="correct__word">
+                                                            {correction.suggestions.map(
+                                                                (
+                                                                    suggestion,
+                                                                    sIndex
+                                                                ) => (
+                                                                    <React.Fragment
+                                                                        key={
+                                                                            sIndex
+                                                                        }
+                                                                    >
+                                                                        <Link to="/write">
+                                                                            {
+                                                                                suggestion.text
+                                                                            }
+                                                                        </Link>
+                                                                        {sIndex !==
+                                                                            correction
+                                                                                .suggestions
+                                                                                .length -
+                                                                                1 && (
+                                                                            <span>
+                                                                                ,
+                                                                                &nbsp;{' '}
+                                                                            </span>
+                                                                        )}
+                                                                    </React.Fragment>
+                                                                )
+                                                            )}
+                                                        </p>
+                                                    </div>
+                                                </li>
+                                            )
+                                        )}
+                                    </ul>
+                                </div>
                             </div>
                         </section>
                     </div>
