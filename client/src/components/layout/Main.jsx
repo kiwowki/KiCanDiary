4aa5873a829c8559c7163822cd7734e1f02f6109
imjonghan <danziro57@gmail.com>
2024-01-29 18:40:03 +0900

    com

--- client/src/components/layout/Main.jsx
+++ client/src/components/layout/Main.jsx
@@ -3,32 +3,38 @@ import { useLocation } from 'react-router-dom'
 import { AnimatePresence, motion } from 'framer-motion'
 const Main = (props) => {
     const location = useLocation()
+    const [isRendered, setIsRendered] = useState(false)
 
-    // 여기서는 간단한 fade 애니메이션을 설정합니다.
     const pageTransition = {
         initial: { opacity: 0 },
         animate: { opacity: 1 },
         exit: { opacity: 0 },
         transition: { duration: 0.7, delay: 0.8, ease: 'easeInOut' },
     }
+
     useEffect(() => {
-        requestAnimationFrame(() => {
-            // 애니메이션을 렌더링 후에 적용합니다.
-        })
-    }, [])
+        setIsRendered(false)
+        const timer = setTimeout(() => {
+            setIsRendered(true)
+        }, 1000) // 1초 후에 렌더링
+
+        // 컴포넌트 unmount 시 타이머 제거
+        return () => clearTimeout(timer)
+    }, [location.pathname])
 
     return (
         <AnimatePresence mode="wait">
-            <motion.main
-                key={location.pathname} // 키를 현재 경로로 설정하여 컴포넌트가 바뀔 때마다 애니메이션을 트리거합니다.
-                variants={pageTransition} // 애니메이션 변형을 정의합니다.
-                initial="initial"
-                animate="animate"
-                exit="exit"
-                id="Main"
-            >
-                {props.children}
-            </motion.main>
+            {isRendered && (
+                <motion.main
+                    key={location.pathname}
+                    variants={pageTransition}
+                    initial="initial"
+                    animate="animate"
+                    id="Main"
+                >
+                    {props.children}
+                </motion.main>
+            )}
         </AnimatePresence>
     )
 }
