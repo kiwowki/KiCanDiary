878fd2c95a53e171f98d93b973151b0de089a6f8
jade1087z <90414378+jade1087z@users.noreply.github.com>
2024-01-30 05:52:45 +0900

    123

--- client/src/components/main/right/TabDiary.jsx
+++ client/src/components/main/right/TabDiary.jsx
@@ -3,6 +3,7 @@ import { getFormattedDate } from '../../../util/calendar/date/dateFormat'
 import diaryList from '../../diary/list/DiaryList'
 import { Link } from 'react-router-dom'
 import monthList from '../../diary/list/MonthList'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const TabDiary = ({ uid, currentDate }) => {
     const [postList, setPostList] = useState([])
@@ -19,17 +20,39 @@ const TabDiary = ({ uid, currentDate }) => {
     useEffect(() => {
         const sortedPosts = monthList(currentDate, postList)
         setFilteredPostList(sortedPosts)
-    }, [postList, currentDate]) // postList의 데이터를 월 별로 관리합니다.
+    }, [postList, currentDate])
 
-    const pageSize = 1 // 한 페이지에 보여줄 아이템의 수
-    const totalPage = 3 // 전체 페이지 수
+    const pageSize = 1
+    const totalPage = 3
 
-    // 현재 페이지에 따라 보여줄 아이템의 시작 인덱스와 끝 인덱스를 계산
     const startIndex = (currentPage - 1) * pageSize
     const endIndex = startIndex + pageSize
-
-    // 시작 인덱스와 끝 인덱스에 따라 아이템을 추출
     const currentItems = filteredPostList.slice(startIndex, endIndex)
+
+    const transition = {
+        hidden: { opacity: 0, x: '100%' },
+        show: { opacity: 1, x: '0%' },
+        exit: { opacity: 0, x: '-100%' }
+    };
+
+    const TabContent = ({ item }) => (
+        <div key={item.postNum} className='diary__tab box2'>
+            <Link to={`/diary/view/${item.postNum}`}>
+                <motion.div className="tab__box" variants={transition} initial="hidden" animate="show" exit="exit" transition={{ duration: 0.3, ease: "easeInOut" }}>
+                    <h4 className="mb20">
+                        {getFormattedDate(item.createdAt).day}
+                        &nbsp;
+                        {getFormattedDate(item.createdAt).month}
+                        ,&nbsp;
+                        {getFormattedDate(item.createdAt).year}
+                    </h4>
+                    <p className="">
+                        {item.content.ops[0].insert}
+                    </p>
+                </motion.div>
+            </Link>
+        </div>
+    );
 
     return (
         <div className="diary__list">
@@ -42,44 +65,30 @@ const TabDiary = ({ uid, currentDate }) => {
                     {Array.from({ length: totalPage }, (_, key) => (
                         <button
                             key={key + 1}
-                            className={`tab__btn mr10 ${
-                                currentPage === key + 1 ? 'active' : ''
-                            }`}
+                            className={`tab__btn mr10 ${currentPage === key + 1 ? 'active' : ''
+                                }`}
                             onClick={() => handlePageChange(key + 1)}
                         >
                             {key + 1}
                         </button>
                     ))}
                 </div>
-                {currentItems && currentItems.length > 0 ? (
-                    currentItems.map((item, key) => (
-                        <div className="diary__tab" key={key}>
-                            <Link to={`/diary/view/${item.postNum}`}>
+                <AnimatePresence mode='wait'>
+                    {currentItems && currentItems.length > 0 ? (
+                        currentItems.map((item, key) => (
+                            <TabContent key={key} item={item} />
+                        ))
+                    ) : (
+                        <div className="diary__tab">
+                            <Link to={'#'}>
                                 <div className="tab__box box2">
-                                    <h4 className="mb20">
-                                        {getFormattedDate(item.createdAt).day}
-                                        &nbsp;
-                                        {getFormattedDate(item.createdAt).month}
-                                        ,&nbsp;
-                                        {getFormattedDate(item.createdAt).year}
-                                    </h4>
-                                    <p className="">
-                                        {item.content.ops[0].insert}
-                                    </p>
+                                    <h4 className="mb20"></h4>
+                                    <p className="">아직 작성된 글이 없습니다.</p>
                                 </div>
                             </Link>
                         </div>
-                    ))
-                ) : (
-                    <div className="diary__tab">
-                        <Link to={'#'}>
-                            <div className="tab__box box2">
-                                <h4 className="mb20"></h4>
-                                <p className="">아직 작성된 글이 없습니다.</p>
-                            </div>
-                        </Link>
-                    </div>
-                )}
+                    )}
+                </AnimatePresence>
             </div>
         </div>
     )
